9f3d101967e9ad234a50b661d317113c36080a4c
stevematdavies <stevematdavies@gmail.com>
2022-11-09 09:35:44 +0200

    Adding delete last tilw functionality

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,6 @@ import '../css/app.css';
 import Alpine from 'alpinejs';
 import game from './game';
 
-Alpine.data('game',() => game)
+Alpine.data('game', () => game)
 Alpine.start();
 
--- resources/js/game.js
+++ resources/js/game.js
@@ -1,79 +1,88 @@
 import Tile from './Tile';
 
 export default {
-  numberOfAllowedGuesses: 3,
-  wordToGuess: "cat",
-  currentRowIndex: 0, 
-  state: 'active',
-  message: '',
-  
-  get currentGuess() {
-      return this.currentRow.map(tile => tile.letter).join('');
-  },
+    numberOfAllowedGuesses: 3,
+    wordToGuess: "cat",
+    currentRowIndex: 0,
+    state: 'active',
+    message: '',
 
-  get currentRow(){
-    return this.gameBoard[this.currentRowIndex];
-  },
+    get currentGuess() {
+        return this.currentRow.map(tile => tile.letter).join('');
+    },
 
-  get remainingGuesses(){
-    return this.numberOfAllowedGuesses === this.currentRowIndex + 1;
-  },
+    get currentRow() {
+        return this.gameBoard[this.currentRowIndex];
+    },
 
-  init(){
-    this.gameBoard = Array.from({ length: this.numberOfAllowedGuesses}, () => 
-      Array.from({length: this.wordToGuess.length}, () => new Tile())
-    ) 
-  },
+    get remainingGuesses() {
+        return this.numberOfAllowedGuesses - this.currentRowIndex - 1;
+    },
 
-  onKeyPressed(key){
-    this.message = "";
-    if (/^[A-z]$/.test(key)){
-      this.fillTileWithKey(key)
-    } else {
-      if (key === "Enter"){
-       this.submitGuess()
-      }
-    }
-  },
-  emptyTile(){},
+    init() {
+        this.gameBoard = Array.from({length: this.numberOfAllowedGuesses}, () =>
+            Array.from({length: this.wordToGuess.length}, () => new Tile())
+        )
+    },
 
+    onKeyPressed(key) {
+        this.message = "";
+        if (/^[A-z]$/.test(key)) {
+            this.fillCurrentTile(key)
+        } else if (key === "Enter") {
+                this.submitGuess()
+        }  else if (key === "Backspace"){
+            this.emptyCurrentTile()
+        }
+    },
 
-  fillTileWithKey(key){
-    for (let tile of this.currentRow){
-      if(!tile.letter){
-       tile.fill(key)
-        break;
-      } 
-    }
-    if (this.currentTileIndex === this.wordToGuess.length -1){
-      this.currentRowIndex++;
-      this.currentTileIndex =0;
-    } else {
-      this.currentTileIndex ++;
-    }
-  },
+    fillCurrentTile(key) {
+        for (let tile of this.currentRow) {
+            if (!tile.letter) {
+                tile.fill(key)
+                break;
+            }
+        }
+        if (this.currentTileIndex === this.wordToGuess.length - 1) {
+            this.currentRowIndex++;
+            this.currentTileIndex = 0;
+        } else {
+            this.currentTileIndex++;
+        }
+    },
 
-  colorTileForCurrentRow(){
-   for (let tile of this.currentRow){
-      tile.updateStatus(this.wordToGuess, this.currentGuess)
-    }
-  },
+    emptyCurrentTile() {
+        for (let tile of [...this.currentRow].reverse()) {
+            if (tile.letter) {
+                tile.empty()
+                break;
+            }
+        }
+    },
 
-  updateMessage(){
-    if (this.currentGuess === this.wordToGuess){
-      this.message = 'Well done, you win!';
-    } else if (this.remainingGuesses){
-        this.message = `You loose.The word was: "${this.wordToGuess}"`;
-        this.state = 'complete'
-      } else {
-        this.message = `...incorrect keep guessing`;
+    colorTileForCurrentRow() {
+        for (let tile of this.currentRow) {
+            tile.updateStatus(this.wordToGuess, this.currentGuess)
+        }
+    },
+
+    updateMessage() {
+        if (this.currentGuess === this.wordToGuess) {
+            this.state = 'complete'
+            return this.message = 'Well done, you win!';
+        }
+        if (this.remainingGuesses === 0) {
+            this.state = 'complete'
+            return this.message = `You loose.The word was: "${this.wordToGuess}"`;
+        }
         this.currentRowIndex++;
-      }
-  },
+        return this.message = `...incorrect keep guessing`;
 
-  submitGuess(){
-      if (this.currentGuess.length < this.wordToGuess.length) return;
-      this.colorTileForCurrentRow();
-      this.updateMessage();
-  }
+    },
+
+    submitGuess() {
+        if (this.currentGuess.length < this.wordToGuess.length) return;
+        this.colorTileForCurrentRow();
+        this.updateMessage();
+    }
 };
