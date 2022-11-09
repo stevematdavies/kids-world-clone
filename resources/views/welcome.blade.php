a9ba521b5b7a86aded120d24c271e8ec3caae46b
stevematdavies <stevematdavies@gmail.com>
2022-11-09 14:27:45 +0200

    Adding some style refactoring

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,16 +10,19 @@
 <body>
 <main x-data="game" @keyup.window="onKeyPressed($event.key)">
 
-    <h1 aria-label="TryCat">
-        <img src="/images/trycat-logo.svg" alt="">
-    </h1>
+    <header>
+        <h1 aria-label="TryCat">
+            <img src="/images/trycat-logo.svg" alt="">
+        </h1>
+        <output x-text="message"></output>
+    </header>
 
     <div id="game">
         <template x-for="(row, index) in gameBoard">
             <div class="row"
                  :class="{
-                 'current': currentRowIndex === index,
-                 'invalid': currentRowIndex === index && errors
+                     'current': currentRowIndex === index,
+                     'invalid': currentRowIndex === index && errors
                  }">
                 <template x-for="tile in row">
                     <div class="tile" :class="tile.status" x-text="tile.letter"></div>
@@ -27,7 +30,7 @@
             </div>
         </template>
     </div>
-    <output x-text="message"></output>
+
 </main>
 </body>
 </html>
